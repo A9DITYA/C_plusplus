4d5e75a7444c3e1498a285d98ff691c85f5e65e6
Aditya Ranjan <116148494+A9DITYA@users.noreply.github.com>
2023-09-08 17:22:45 +0530

    Add files via upload

---
+++ user_input.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+void sum()
+{
+	int x,y,sum;
+	cout<<"Enter your first number:\n";
+	cin>>x;
+	cout<<"Enter your second number:\n";
+	cin>>y;
+	sum=x+y;
+	cout<<"The sum of two number is:\n" <<sum;
+	
+	
+}
+int main()
+{
+	sum();
+	return 0;
+}
